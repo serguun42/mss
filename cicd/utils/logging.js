d25df1126646e08c7421a28af22d1c3924c8c6af
serguun42 <s42@serguun42.ru>
2021-03-25 12:38:17 +0300

    Created CI/CD branch/folder

---
+++ cicd/utils/logging.js
@@ -0,0 +1,37 @@
+const
+	DEV = require("os").platform() === "win32" || process.argv[2] === "DEV",
+	{
+		LOGGING_PORT,
+		LOGGING_TAG
+	} = DEV ? require("../../../DEV_CONFIGS/ci.config.json") : require("../ci.config.json"),
+	NodeFetch = require("node-fetch");
+
+
+/**
+ * @param {(Error | String)[]} args
+ * @returns {void}
+ */
+const Logging = (...args) => {
+	const payload = {
+		error: args.findIndex((message) => message instanceof Error) > -1,
+		args: args,
+		tag: LOGGING_TAG
+	};
+
+	NodeFetch(`http://127.0.0.1:${LOGGING_PORT}`, {
+		method: "POST",
+		body: JSON.stringify(payload)
+	}).then((res) => {
+		if (res.status !== 200)
+			return res.text().then((text) => {
+				console.warn(new Date());
+				console.warn(`Status code = ${res.status}`);
+				console.warn(text);
+			});
+	}).catch((e) => {
+		console.warn(new Date());
+		console.warn(e);
+	});
+};
+
+module.exports = Logging;
