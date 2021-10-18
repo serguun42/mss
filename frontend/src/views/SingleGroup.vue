cd9a6c9c61e77e8a3fccc0d789fa1d8dbb0964f2
serguun42 <s42@serguun42.ru>
2021-10-19 00:36:38 +0300

    Fix date formats in SignleGroup and Stats views

--- frontend/src/views/SingleGroup.vue
+++ frontend/src/views/SingleGroup.vue
@@ -58,12 +58,12 @@ export default {
 			if (isNaN(parsedUpdatedDate.getTime())) return null;
 
 			if (Math.floor((parsedUpdatedDate.getTime() - new Date().getTimezoneOffset() * 60e3) / 86400e3) === Math.floor((Date.now() - new Date().getTimezoneOffset() * 60e3) / 86400e3))
-				return `сегодня, ${parsedUpdatedDate.getHours()}:${parsedUpdatedDate.getHours().toString().padStart(2, "0")}`;
+				return `сегодня, ${parsedUpdatedDate.getHours()}:${parsedUpdatedDate.getMinutes().toString().padStart(2, "0")}`;
 			
 			if (Math.floor((parsedUpdatedDate.getTime() - new Date().getTimezoneOffset() * 60e3) / 86400e3) === Math.floor((Date.now() - new Date().getTimezoneOffset() * 60e3) / 86400e3) - 1)
-				return `вчера, ${parsedUpdatedDate.getHours()}:${parsedUpdatedDate.getHours().toString().padStart(2, "0")}`;
+				return `вчера, ${parsedUpdatedDate.getHours()}:${parsedUpdatedDate.getMinutes().toString().padStart(2, "0")}`;
 
-			return `${parsedUpdatedDate.toLocaleDateString()}, ${parsedUpdatedDate.getHours()}:${parsedUpdatedDate.getHours().toString().padStart(2, "0")}`;
+			return `${parsedUpdatedDate.toLocaleDateString()}, ${parsedUpdatedDate.getHours()}:${parsedUpdatedDate.getMinutes().toString().padStart(2, "0")}`;
 		}
 	},
 	data() {
