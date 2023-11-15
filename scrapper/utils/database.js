6e2daba04ab337a9ae53d6290b9507d25eb829bc
serguun42 <s42@serguun42.ru>
2023-11-16 02:10:53 +0300

    Use Docker for deploy

--- scrapper/utils/database.js
+++ scrapper/utils/database.js
@@ -1,10 +1,11 @@
-const
-	Logging = require("./logging"),
-	DEV = require("os").platform() === "win32" || process.argv[2] === "DEV",
-	{ DATABASE_CONNECTION_URI } = DEV ? require("../../../DEV_CONFIGS/scrapper.config.json") : require("../scrapper.config.json"),
-	mongoClient = require("mongodb").MongoClient,
-	MONGO_CONNECTION_OPTIONS = {},
-	MONGO_URL = DATABASE_CONNECTION_URI || "mongodb://127.0.0.1:27017/";
+const Logging = require("./logging");
+const DEV = require("os").platform() === "win32" || process.argv[2] === "DEV";
+const { DATABASE_CONNECTION_URI } = DEV
+  ? require("../../../DEV_CONFIGS/scrapper.config.json")
+  : require("../scrapper.config.json");
+const mongoClient = require("mongodb").MongoClient;
+const MONGO_CONNECTION_OPTIONS = {};
+const MONGO_URL = DATABASE_CONNECTION_URI || "mongodb://127.0.0.1:27017/";
 
 /**
  * @typedef {import("mongodb").Db} DB
@@ -19,98 +20,88 @@ const
  * @classdesc Various events and callbacks for DB
  */
 class MongoDispatcher {
-	/**
-	 * @param {string} iDatabaseName
-	 */
-	constructor(iDatabaseName) {
-		/**
-		 * @private
-		 * @type {DB}
-		 */
-		this.DB = null;
+  /**
+   * @param {string} iDatabaseName
+   */
+  constructor(iDatabaseName) {
+    /**
+     * @private
+     * @type {DB}
+     */
+    this.DB = null;
 
+    /**
+     * @private
+     * @type {{[eventName: string]: MongoDispatcherCallback[]}}
+     */
+    this.events = {};
 
-		/**
-		 * @private
-		 * @type {{[eventName: string]: MongoDispatcherCallback[]}}
-		 */
-		this.events = {};
+    mongoClient.connect(MONGO_URL, MONGO_CONNECTION_OPTIONS, (mongoError, mongoConnection) => {
+      if (mongoError) {
+        Logging("Error with connection to MongoDB on start-up", mongoError);
+      } else {
+        this.DB = mongoConnection.db(iDatabaseName);
 
+        this.on("close", () => {
+          this.DB = null;
+          mongoConnection.close();
+        });
+      }
+    });
+  }
 
-		mongoClient.connect(MONGO_URL, MONGO_CONNECTION_OPTIONS, (mongoError, mongoConnection) => {
-			if (mongoError) {
-				Logging("Error with connection to MongoDB on start-up", mongoError);
-			} else {
-				this.DB = mongoConnection.db(iDatabaseName);
+  /**
+   * @param {string} iEventName
+   * @param {MongoDispatcherCallback} iOnEventHandler
+   * @returns {void}
+   */
+  on(iEventName, iOnEventHandler) {
+    if (!this.events[iEventName] || !(this.events[iEventName] instanceof Array)) this.events[iEventName] = [];
 
-				this.on("close", () => {
-					this.DB = null;
-					mongoConnection.close();
-				});
-			};
-		});
-	}
+    this.events[iEventName].push(iOnEventHandler);
+  }
 
+  /**
+   * @param {string} iEventName
+   * @returns {void}
+   */
+  off(iEventName) {
+    delete this.events[iEventName];
+  }
 
+  /**
+   * @param {string} iEventName
+   * @returns {void}
+   */
+  dispatchEvent(iEventName) {
+    if (this.events[iEventName] && this.events[iEventName] instanceof Array)
+      this.events[iEventName].forEach((eventHandler) => {
+        if (typeof eventHandler == "function") eventHandler(this.DB);
+      });
+  }
 
-	/**
-	 * @param {string} iEventName
-	 * @param {MongoDispatcherCallback} iOnEventHandler
-	 * @returns {void}
-	 */
-	on(iEventName, iOnEventHandler) {
-		if (!this.events[iEventName] || !(this.events[iEventName] instanceof Array))
-			this.events[iEventName] = [];
+  /**
+   * @returns {void}
+   */
+  closeConnection() {
+    this.dispatchEvent("close");
+  }
 
-		this.events[iEventName].push(iOnEventHandler);
-	}
+  /**
+   * @returns {Promise<DB>}
+   */
+  callDB() {
+    return new Promise((resolve) => {
+      if (this.DB) return resolve(this.DB);
 
-	/**
-	 * @param {string} iEventName
-	 * @returns {void}
-	 */
-	off(iEventName) {
-		delete this.events[iEventName];
-	}
-
-	/**
-	 * @param {string} iEventName
-	 * @returns {void}
-	 */
-	dispatchEvent(iEventName) {
-		if (this.events[iEventName] && this.events[iEventName] instanceof Array)
-			this.events[iEventName].forEach((eventHandler) => {
-				if (typeof eventHandler == "function")
-					eventHandler(this.DB);
-			});
-	}
-
-
-
-	/**
-	 * @returns {void}
-	 */
-	closeConnection() {
-		this.dispatchEvent("close");
-	}
-
-
-
-	/**
-	 * @returns {Promise<DB>}
-	 */
-	callDB() {
-		return new Promise((resolve) => {
-			if (this.DB) return resolve(this.DB);
-
-			const waitingInterval = setInterval(() => {
-				if (this.DB) {
-					clearInterval(waitingInterval);
-					resolve(this.DB);
-				};
-			});
-		})
-	}
-};
+      const waitingInterval = setInterval(() => {
+        if (this.DB) {
+          clearInterval(waitingInterval);
+          resolve(this.DB);
+        }
+      });
+    });
+  }
+}
 
 module.exports = MongoDispatcher;
--- telegram/utils/logging.js
+++ telegram/utils/logging.js
@@ -1,37 +1,37 @@
-const
-	DEV = require("os").platform() === "win32" || process.argv[2] === "DEV",
-	{
-		LOGGING_PORT,
-		LOGGING_TAG
-	} = DEV ? require("../../../DEV_CONFIGS/telegram-bot.config.json") : require("../telegram-bot.config.json"),
-	NodeFetch = require("node-fetch");
+const fetch = require("node-fetch");
 
+const DEV = require("os").platform() === "win32" || process.argv[2] === "DEV";
+const { LOGGING_HOST, LOGGING_PORT, LOGGING_TAG } = DEV
+  ? require("../../../DEV_CONFIGS/telegram-bot.config.json")
+  : require("../telegram-bot.config.json");
 
 /**
  * @param {(Error | String)[]} args
  * @returns {void}
  */
 const Logging = (...args) => {
-	const payload = {
-		error: args.findIndex((message) => message instanceof Error) > -1,
-		args: args.map((arg) => arg instanceof Error ? arg?.message || arg : arg),
-		tag: LOGGING_TAG
-	};
+  const payload = {
+    error: args.findIndex((message) => message instanceof Error) > -1,
+    args: args.map((arg) => (arg instanceof Error ? arg?.message || arg : arg)),
+    tag: LOGGING_TAG
+  };
 
-	NodeFetch(`http://127.0.0.1:${LOGGING_PORT}`, {
-		method: "POST",
-		body: JSON.stringify(payload)
-	}).then((res) => {
-		if (res.status !== 200)
-			return res.text().then((text) => {
-				console.warn(new Date());
-				console.warn(`Status code = ${res.status}`);
-				console.warn(text);
-			});
-	}).catch((e) => {
-		console.warn(new Date());
-		console.warn(e);
-	});
+  fetch(`http://${LOGGING_HOST}:${LOGGING_PORT}`, {
+    method: "POST",
+    body: JSON.stringify(payload)
+  })
+    .then((res) => {
+      if (res.status !== 200)
+        return res.text().then((text) => {
+          console.warn(new Date());
+          console.warn(`Status code = ${res.status}`);
+          console.warn(text);
+        });
+    })
+    .catch((e) => {
+      console.warn(new Date());
+      console.warn(e);
+    });
 };
 
 module.exports = DEV ? console.log : Logging;
